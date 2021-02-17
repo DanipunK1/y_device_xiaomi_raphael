2d6e213e3457a40a4cec1c503fd776fde26e088d
DanipunK1 <pegate.eltrueco@gmail.com>
2021-02-17 22:41:23 +0100

    use vibrator hal from source

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,6 @@
 rm -rf hardware/qcom-caf/sm8150/media
-rm -rf vendor/qcom/opensource/vibrator
 rm -rf packages/apps/CherishSettings
 git clone https://github.com/ralf979/hardware_qcom-caf_sm8150_media hardware/qcom-caf/sm8150/media
-git clone https://github.com/ralf979/vendor_qcom_opensource_vibrator.git -b eleven vendor/qcom/opensource/vibrator
 git clone https://github.com/DanipunK1/android_packages_apps_Cherish.git -b eleven packages/apps/CherishSettings
 
 export SKIP_ABI_CHECKS=true
