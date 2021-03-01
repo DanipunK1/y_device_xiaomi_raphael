475092dd4e7b8225fa19d185940a57e3a8be8937
DanipunK1 <pegate.eltrueco@gmail.com>
2021-03-01 17:17:36 +0100

    fix fetch error in vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 rm -rf hardware/qcom-caf/sm8150/media
 git clone https://github.com/ralf979/hardware_qcom-caf_sm8150_media hardware/qcom-caf/sm8150/media
 cd packages/apps/CherishSettings
-git fetch https://github.com/DanipunK1/android_packages_apps_Cherish -b eleven && git cherry-pick 192fb098c76492a5b9857e3d6194a96d937093be
+git fetch https://github.com/DanipunK1/android_packages_apps_Cherish eleven && git cherry-pick 192fb098c76492a5b9857e3d6194a96d937093be
 cd ../../..
 
 export SKIP_ABI_CHECKS=true
