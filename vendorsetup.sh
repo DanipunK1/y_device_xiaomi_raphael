b59132bf330bdf612e4cad4cbdb7f511a26c7e48
DanipunK1 <pegate.eltrueco@gmail.com>
2021-03-07 00:23:08 +0100

    update vs.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf hardware/qcom-caf/sm8150/media
-git clone https://github.com/ralf979/hardware_qcom-caf_sm8150_media hardware/qcom-caf/sm8150/media
+git clone https://github.com/DanipunK1/hardware_qcom-caf_sm8150_media -b eleven hardware/qcom-caf/sm8150/media
 cd packages/apps/CherishSettings
 git fetch https://github.com/DanipunK1/android_packages_apps_Cherish eleven && git cherry-pick 192fb098c76492a5b9857e3d6194a96d937093be
 cd ../../..
@@ -7,6 +7,5 @@ cd ../../..
 export SKIP_ABI_CHECKS=true
 export WITH_GAPPS=true
 export TARGET_GAPPS_ARCH=arm64
-export TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 
