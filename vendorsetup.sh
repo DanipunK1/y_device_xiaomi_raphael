b3f53a158d18d66110a0662ed0892959336c6749
DanipunK1 <pegate.eltrueco@gmail.com>
2021-03-08 13:44:43 +0100

    add dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,3 @@
-rm -rf hardware/qcom-caf/sm8150/media
-git clone https://github.com/DanipunK1/hardware_qcom-caf_sm8150_media -b eleven hardware/qcom-caf/sm8150/media
-cd packages/apps/CherishSettings
-git fetch https://github.com/DanipunK1/android_packages_apps_Cherish eleven && git cherry-pick 192fb098c76492a5b9857e3d6194a96d937093be
-cd ../../..
-
 export SKIP_ABI_CHECKS=true
 export WITH_GAPPS=true
 export TARGET_GAPPS_ARCH=arm64
